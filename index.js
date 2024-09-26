c041e37259e190b4e95e1ed48313e5acec663867
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2024-09-26 16:44:42 +0200

    fix: error with redirect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -20,6 +21,10 @@ app.use( express.json() );
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 
 //Escuchar peticiones
